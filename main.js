995b0013fcd22e64461f65a83a1c6e4d1d546234
Jack <43762306+JackRKRACT@users.noreply.github.com>
2021-04-28 17:25:38 -0500

    Cleaned up some old code as well

--- main.js
+++ main.js
@@ -3,17 +3,10 @@ const fs = require('fs');
 const prompt = require('prompt-sync')();
 const Match = require("./match");
 
-/*
-// Update April 28th : Not tracking your wins in new version
-var yourWins = 0;
-var yourLoss = 0;
- */
-
 var currFile = 0;
 var skippedParsing = 0;
 var matchArray = [];
 var totalFile;
-
 
 console.clear();
 const replayDirectory = prompt('Replay directory : ');//"./replays/";
@@ -82,12 +75,6 @@ function readDirectory(directoryName) {
 }
 
 function calculateInfo(files) {
-    /*
-    process.stdout.clearLine();
-    process.stdout.cursorTo(0);
-    process.stdout.write(`Progess : ${currFile}/${totalFile} | [${strCurrent}] `);
-    */
-
     files.forEach(file => {
         updateProgressBar();
         currFile++;
@@ -146,102 +133,3 @@ function updateProgressBar() {
     process.stdout.cursorTo(0);
     process.stdout.write(`Progess : [${strCurrent}${strEmpty}] | ${currFile}/${totalFile}`);
 }
-
-/*
-// Update April 28th : Not using this function anymore, gonna parse all files in directory.
-// Filter results to two player codes
-for (let l = 0; l < files.length; l++) {
-    //https://www.npmjs.com/package/fs-backwards-stream if I wanna give in to backwards streams
-    let current = files[l];
-    let filecontents = fs.readFileSync((replayDirectory + current), 'utf8');
-
-    let lines = filecontents.toString().split('\n');
-    //console.log(lines.length);
-    let short1 = yourCode.toString().split("#")[0];
-    let short2 = opponentCode.toString().split("#")[0];
-    //console.log(short1 + " " + short2);
-    let valid = false;
-    let valid2 = false;
-
-    for (let k = lines.length-1; k > lines.length-4; k--) {
-        //console.log("This is the number" + k);
-        let currentLine = lines[k].toString();
-        if (currentLine.includes(short1)) {
-            //console.log("We made it here?");
-            valid = true;
-        }
-        if (currentLine.includes(short2)) {
-            valid2 = true;
-        }
-        if (valid && valid2) {
-            k = (lines.length-3);
-        }
-    }
-
-    if (!valid || !valid2) {
-        files.splice(l, 1);
-        l--;
-    }
-}
-
-console.log("Filtered .slp files to two player codes.");
-
- */
-
-/*
-// Update April 28th : Not gonna be using player specific or character specific filtering now, parsing all files.
-const yourCode = prompt('Your code : ');
-console.clear();
-const opponentCode = prompt('Opponent code : ');
-console.clear();
-const yourCharacterID = Number(prompt('Your character ID : '));
-console.clear();
-const opponentCharacterID = Number(prompt('Opponent character ID : ')); // Sheik 19 Falcon 0
-console.clear();
-
- */
-
-/*
-// Update April 28th : Not looking for just win loss anymore, removing this method.
-else {
-            const players = metaData.players;
-            const stats = current_game.getStats();
-            const settings = current_game.getSettings();
-            if (players[0].names.code === yourCode && players[1].names.code === opponentCode) {
-                //console.log("Your code is player 1 in this game!");
-                // User code is player 1. Opponent code matches desired
-                if (settings.players[0].characterId == yourCharacterID && settings.players[1].characterId == opponentCharacterID) {
-                    let result1 = determineWin(stats.overall, yourCode, 0);
-                    //console.log("We got a match! Result is " + result1);
-                    if (result1) {
-                        yourWins++;
-                    } else {
-                        yourLoss++;
-                    }
-                } else {
-                    skippedParsing++;
-                }
-            } else if (players[1].names.code === yourCode && players[0].names.code === opponentCode) {
-                // User code is player 2. Opponent code matches desired.
-                //console.log("Match pt1 for port 2.");
-                if (settings.players[1].characterId === yourCharacterID && settings.players[0].characterId === opponentCharacterID) {
-                    let result2 = determineWin(stats.overall, yourCode, 1);
-                    //console.log("We got a match! Result is " + result2);
-                    if (result2) {
-                        yourWins++;
-                    } else {
-                        yourLoss++;
-                    }
-                } else {
-                    skippedParsing++;
-                }
-            } else {
-                // Player code does not match for both users. Skipping
-                skippedParsing++;
-            }
-        }
- */
-
-/* Refactored out for final product
-console.log("Wins : " + yourWins + " | Losses : " + yourLoss + " | Skipped : " + skippedParsing);
-*/
